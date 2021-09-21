da0c41254559ccc1ca0e2e4306377a6113553103
99063871 <99063871@mydavinci.com>
2021-09-21 11:10:47 +0200

    elif-statement

--- programma.py
+++ programma.py
@@ -1,7 +1,11 @@
 a = int(60)
-b = int(2)
+b = int(62)
 
 
 if a>b:
     Max =str(a)
     print('a is het grootste getal: ' + Max)
+
+elif a<b:
+    Min = str(a)
+    print('a is het kleinste getal: ' + Min)
