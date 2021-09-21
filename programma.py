34168fb3cbdc172a0c992222d52b202838904b73
99063871 <99063871@mydavinci.com>
2021-09-21 11:16:21 +0200

    Min en Max

--- programma.py
+++ programma.py
@@ -1,14 +1,17 @@
-a = int(60)
-b = int(60)
+a = int(65)
+b = int(62)
 
+Max = str(a)
+Min = str(a)
 
 if a>b:
-    Max =str(a)
     print('a is het grootste getal: ' + Max)
 
 elif a<b:
-    Min = str(a)
     print('a is het kleinste getal: ' + Min)
 
 else:
     print("a en b zijn even groot")
+
+print("Het minimum is: " + Min)
+print("Het maximum is: " + Max)
